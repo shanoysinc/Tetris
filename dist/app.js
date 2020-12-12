9d8c0c8979388e0f81eb8a33f3bd34b240d95db5
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-11 21:38:12 -0800

    add tetromino to board

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,77 @@ document.addEventListener("DOMContentLoaded", () => {
     function generateGrid() {
         for (let i = 0; i < 210; i++) {
             const divElement = document.createElement("div");
-            if (i > 200) {
+            if (i >= 200) {
                 divElement.classList.add("taken");
             }
             grid?.appendChild(divElement);
         }
     }
     generateGrid();
+    const squareWidth = 10;
+    const lTeromino = [
+        [1, squareWidth + 1, squareWidth * 2 + 1, 2],
+        [squareWidth, squareWidth + 1, squareWidth + 2, squareWidth * 2 + 2],
+        [1, squareWidth + 1, squareWidth * 2 + 1, squareWidth * 2],
+        [
+            squareWidth,
+            squareWidth + 2,
+            squareWidth * 2 + 1,
+            squareWidth * 2 + 2,
+        ],
+    ];
+    const zTetromino = [
+        [0, squareWidth, squareWidth + 1, squareWidth * 2 + 1],
+        [
+            squareWidth + 1,
+            squareWidth + 2,
+            squareWidth * 2,
+            squareWidth * 2 + 1,
+        ],
+        [0, squareWidth, squareWidth + 1, squareWidth * 2 + 1],
+        [
+            squareWidth + 1,
+            squareWidth + 2,
+            squareWidth * 2,
+            squareWidth * 2 + 1,
+        ],
+    ];
+    const tTetromino = [
+        [1, squareWidth, squareWidth + 1, squareWidth + 2],
+        [1, squareWidth + 1, squareWidth + 2, squareWidth * 2 + 1],
+        [squareWidth, squareWidth + 1, squareWidth + 2, squareWidth * 2 + 1],
+        [1, squareWidth, squareWidth + 1, squareWidth * 2 + 1],
+    ];
+    const oTetromino = [
+        [0, 1, squareWidth, squareWidth + 1],
+        [0, 1, squareWidth, squareWidth + 1],
+        [0, 1, squareWidth, squareWidth + 1],
+        [0, 1, squareWidth, squareWidth + 1],
+    ];
+    const iTetromino = [
+        [1, squareWidth + 1, squareWidth * 2 + 1, squareWidth * 3 + 1],
+        [squareWidth, squareWidth + 1, squareWidth + 2, squareWidth + 3],
+        [1, squareWidth + 1, squareWidth * 2 + 1, squareWidth * 3 + 1],
+        [squareWidth, squareWidth + 1, squareWidth + 2, squareWidth + 3],
+    ];
+    const TetrominoShapes = [
+        lTeromino,
+        zTetromino,
+        tTetromino,
+        oTetromino,
+        iTetromino,
+    ];
+    let randomTetromino = Math.floor(Math.random() * TetrominoShapes.length);
+    const colors = ["orange", "yellow", "green", "purple", "red"];
+    let currentPosition = 3;
+    let currentTetromino = TetrominoShapes[randomTetromino][0];
+    const squares = document.querySelectorAll("#grid div");
+    function drawTetromino() {
+        currentTetromino.forEach((index) => {
+            console.log(index);
+            squares[index + currentPosition].style.backgroundColor =
+                colors[randomTetromino];
+        });
+    }
+    drawTetromino();
 });
