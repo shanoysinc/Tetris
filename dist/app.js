7e8576d5c825189dc0f1b2f63d42f991ee9267fa
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-11 21:01:39 -0800

    styling the grid

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("helo");
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.querySelector("#grid");
+    function generateGrid() {
+        for (let i = 0; i < 210; i++) {
+            const divElement = document.createElement("div");
+            if (i > 200) {
+                divElement.classList.add("taken");
+            }
+            grid?.appendChild(divElement);
+        }
+    }
+    generateGrid();
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("helo");
+document.addEventListener("DOMContentLoaded", () => {
+	const grid = document.querySelector("#grid");
+	function generateGrid() {
+		for (let i = 0; i < 210; i++) {
+			const divElement = document.createElement("div");
+			if (i > 200) {
+				divElement.classList.add("taken");
+			}
+			grid?.appendChild(divElement);
+		}
+	}
+
+	generateGrid();
+});
